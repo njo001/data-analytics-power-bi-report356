76acfff61d44f4a99c1a50c95b702f06256b17bc
njo001 <nicola.johnstone@surrey.ac.uk>
2024-08-09 07:00:49 +0100

    Code for question 3.

--- question_3.sql
+++ question_3.sql
@@ -1,1 +1,15 @@
--- Which German store type had the highest revenue for 2022?
+-- Which German store type had the highest revenue for 2022? Local   
+
+SELECT
+    store_type,
+    SUM(sale_price - cost_price) AS revenue
+FROM 
+    forquerying2
+WHERE 
+    country = 'Germany' 
+    AND EXTRACT(YEAR FROM TO_DATE(dates, 'YYYY/MM/DD')) = 2022
+GROUP BY
+    store_type
+ORDER BY 
+    revenue DESC
+LIMIT 1;
