2f2df4931a434fc65f54f3de3f7177142b183240
njo001 <nicola.johnstone@surrey.ac.uk>
2024-08-09 06:47:20 +0100

    Code for question 2 complete.

--- question_2.sql
+++ question_2.sql
@@ -1,1 +1,16 @@
--- Which month in 2022 has had the highest revenue?
+-- Which month in 2022 has had the highest revenue? October
+
+SELECT 
+    month_name AS month, 
+    SUM(sale_price - cost_price) AS revenue
+FROM 
+    forquerying2
+WHERE 
+    EXTRACT(YEAR FROM TO_DATE(dates, 'YYYY/MM/DD')) = 2022
+GROUP BY 
+    month
+ORDER BY 
+    revenue DESC   
+LIMIT 1;    
+
+
