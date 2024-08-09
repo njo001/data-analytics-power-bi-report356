4e846eb76d123b6e1ce86f12a54c19b8256d870c
njo001 <nicola.johnstone@surrey.ac.uk>
2024-08-09 08:10:10 +0100

    Code for question 5.

--- question_5.sql
+++ question_5.sql
@@ -1,1 +1,16 @@
 -- Which product category generated the most profit for the "Wiltshire, UK" region in 2021?
+-- Homeware
+
+SELECT 
+    category,
+    SUM(sale_price - cost_price) AS profit
+FROM 
+    forquerying2
+WHERE 
+    full_region = 'Wiltshire, UK'
+    AND EXTRACT(YEAR FROM TO_DATE(dates, 'YYYY/MM/DD')) = 2021
+GROUP BY
+    category 
+ORDER BY 
+    profit DESC   
+LIMIT 1;    
