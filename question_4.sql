bc169de7fdc9af0be9b036525362bf4c2909a1d9
njo001 <nicola.johnstone@surrey.ac.uk>
2024-08-09 07:45:37 +0100

    Code for question 4.

--- question_4.sql
+++ question_4.sql
@@ -1,1 +1,14 @@
 -- Create a view where the rows are the store types and the columns are the total sales, percentage of total sales and the count of orders
+
+CREATE VIEW store_sales AS
+SELECT 
+    store_type,
+    SUM(sale_price * product_quantity) AS total_sales,
+    (SUM(sale_price * product_quantity)/ SUM(SUM(sale_price*product_quantity))OVER())*100 AS percentage_total_sales,
+    SUM(product_quantity) AS order_count
+FROM 
+    forquerying2
+GROUP BY
+    store_type;   
+
+
